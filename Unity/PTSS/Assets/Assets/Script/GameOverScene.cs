211d9ae835bdb9788606e167412a74054fb01c16
kaaaaaaaaaaarl <caalis18@gmail.com>
2023-06-25 12:16:36 +0200

    shopDoneMergeDone

---
+++ Unity/PTSS/Assets/Assets/Script/GameOverScene.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameOverScene : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public GameObject gameOverBar;
+    public GameObject HealthMonitor;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    void OnTriggerEnter2D(Collider2D col)
+    {
+
+        if (col.name  == gameOverBar.name) {
+            Debug.Log("dammage delt");
+            //moneythingy.GetComponent<moneyThing>().removeMoney(OvenPrice);
+            HealthMonitor.GetComponent<HealthMonitor>().removeHealth(1);
+
+        }
+    }
+    }
---
+++ Unity/PTSS/Assets/Assets/Script/HealthMonitor.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HealthMonitor : MonoBehaviour
+{
+    public int health;
+    public void addHealth (int healthBack)
+    {
+        health = health + healthBack;
+    }
+    public void removeHealth(int dammage)
+    {
+        health = health - dammage;
+    }
+}
